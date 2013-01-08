44c20bed2f4ed062286614faba6cb68a8a02ed9b
Kees van Bochove <kees@thehyve.nl>
2013-01-08 18:34:23 +0100

    added bash shell script

---
+++ scripts/startup.sh
@@ -0,0 +1,10 @@
+#:: Command to start the script
+#:: Usage: startup.sh [optional target_name]
+#:: Examples:
+#::   startup.sh
+#::   startup.sh setup
+#::   startup.sh clean
+#::   startup.sh install
+#:: If necessary, grant executable rights with chmod +x startup.sh
+#::
+java -classpath "../lib/commons-cli/commons-cli/jars/commons-cli-1.2.jar:../lib/commons-cli/commons-cli/jars/commons-cli-1.2.jar:../lib/org.apache.ant/ant-launcher/jars/ant-launcher-1.8.2.jar:../lib/org.apache.ant/ant/jars/ant-1.8.2.jar:../lib/org.codehaus.groovy/groovy-all/jars/groovy-all-1.8.6.jar:../lib/org.codehaus.gant/gant_groovy1.8/jars/gant_groovy1.8-1.9.6.jar" org.codehaus.groovy.tools.GroovyStarter --main gant.Gant --file setupTranSMARTDevelopment.gant $1
