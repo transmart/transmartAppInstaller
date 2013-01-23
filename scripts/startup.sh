097692241ff4bb8311b28f7a92e5e2e0a8d7f7f5
keesvanbochove <kees@thehyve.nl>
2013-01-23 13:29:04 +0100

    Add sh cue to startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #:: Command to start the script
 #:: Usage: startup.sh [optional target_name]
 #:: Examples:
@@ -7,4 +8,4 @@
 #::   startup.sh install
 #:: If necessary, grant executable rights with chmod +x startup.sh
 #::
-java -classpath "../lib/commons-cli/commons-cli/jars/commons-cli-1.2.jar:../lib/commons-cli/commons-cli/jars/commons-cli-1.2.jar:../lib/org.apache.ant/ant-launcher/jars/ant-launcher-1.8.2.jar:../lib/org.apache.ant/ant/jars/ant-1.8.2.jar:../lib/org.codehaus.groovy/groovy-all/jars/groovy-all-1.8.6.jar:../lib/org.codehaus.gant/gant_groovy1.8/jars/gant_groovy1.8-1.9.6.jar" org.codehaus.groovy.tools.GroovyStarter --main gant.Gant --file setupTranSMARTDevelopment.gant $1
+java -classpath "../lib/postgresql91-jdbc.jar:../lib/commons-cli/commons-cli/jars/commons-cli-1.2.jar:../lib/commons-cli/commons-cli/jars/commons-cli-1.2.jar:../lib/org.apache.ant/ant-launcher/jars/ant-launcher-1.8.2.jar:../lib/org.apache.ant/ant/jars/ant-1.8.2.jar:../lib/org.codehaus.groovy/groovy-all/jars/groovy-all-1.8.6.jar:../lib/org.codehaus.gant/gant_groovy1.8/jars/gant_groovy1.8-1.9.6.jar" org.codehaus.groovy.tools.GroovyStarter --main gant.Gant --file setupTranSMARTDevelopment.gant $1
