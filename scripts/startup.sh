56e7db3ce653b48805f35116002aa45b7d49f8c8
Gustavo Lopes <gustavo@thehyve.nl>
2013-05-31 16:19:52 +0200

    Omnibus commit: simplified config and robustness
    - Using now just one sample configuration; no point in having one per
      platform as the adapations needed are obvious.
    - Path for git and psql derived from $PATH if not specified.
    - Path for ant defaulting to builtin ant.
    - New optional configuration settings: postres.password, postgres.host
      and postgres.port.
    - All postgres options are optional.
    - Removed deprecated installation of RModules.
    - A lot of sanity checks now done on startup.
    - Cleanup of build dir moved to separate target, as it's not a good idea
      to be downloading the git repositories every time – a hard reset
      should suffice (we don't do a clean, but we could do that as well).
    - Clean target now fails if the database cannot be dropped.
    - Some roles were not being deleted on the clean target; fixed.
    - Make dump load work with current version of transmartApp-DB.
    - Fixed encoding and collation used when creating transmart database.
    - Use 1777 permissions instead of chown on tablespace directories, as
      that allows the script to better be run as not root.
    - Improved startup.sh to honout JAVA_HOME.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 #:: Command to start the script
 #:: Usage: startup.sh [optional target_name]
 #:: Examples:
@@ -8,4 +9,23 @@
 #::   startup.sh install
 #:: If necessary, grant executable rights with chmod +x startup.sh
 #::
-java -classpath "../lib/postgresql91-jdbc.jar:../lib/commons-cli/commons-cli/jars/commons-cli-1.2.jar:../lib/commons-cli/commons-cli/jars/commons-cli-1.2.jar:../lib/org.apache.ant/ant-launcher/jars/ant-launcher-1.8.2.jar:../lib/org.apache.ant/ant/jars/ant-1.8.2.jar:../lib/org.codehaus.groovy/groovy-all/jars/groovy-all-1.8.6.jar:../lib/org.codehaus.gant/gant_groovy1.8/jars/gant_groovy1.8-1.9.6.jar" org.codehaus.groovy.tools.GroovyStarter --main gant.Gant --file setupTranSMARTDevelopment.gant $1
+#:: Current directory should be script's directory
+
+if [ -z "$JAVA_HOME" ]; then
+	JAVA_LOCATION=`which java`
+	if [ -n "$JAVA_LOCATION" ]; then
+		JAVA_HOME=`readlink -m '$JAVA_LOCATION' | xargs dirname | xargs dirname | xargs dirname`
+	fi
+	if [ -z "$JAVA_HOME" ]; then
+		echo "Could not find java. Define JAVA_HOME"
+		exit 1
+	fi
+fi
+
+if [ ! -f "$JAVA_HOME/bin/java" ]; then
+	echo "Could not find java. Define JAVA_HOME"
+	exit 1
+fi
+
+export JAVA_HOME
+"$JAVA_HOME/bin/java" -classpath "../lib/postgresql91-jdbc.jar:../lib/commons-cli/commons-cli/jars/commons-cli-1.2.jar:../lib/commons-cli/commons-cli/jars/commons-cli-1.2.jar:../lib/org.apache.ant/ant-launcher/jars/ant-launcher-1.8.2.jar:../lib/org.apache.ant/ant/jars/ant-1.8.2.jar:../lib/org.codehaus.groovy/groovy-all/jars/groovy-all-1.8.6.jar:../lib/org.codehaus.gant/gant_groovy1.8/jars/gant_groovy1.8-1.9.6.jar" org.codehaus.groovy.tools.GroovyStarter --main gant.Gant --file setupTranSMARTDevelopment.gant $@
